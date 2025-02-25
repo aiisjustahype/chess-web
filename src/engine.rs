02a14d962f6f683aa6cb7de6825151eb06dfccb5
aiisjustahype <N/A@>
2025-02-25 15:16:05 +0100

    bug fixes and quiescence search

--- src/engine.rs
+++ src/engine.rs
@@ -25,8 +25,8 @@ const BISHOP_SQUARE_RATINGS: [i32; 64] = [
 const ROOK_SQUARE_RATINGS: [i32; 64] = [
     500, 500, 500, 500, 500, 500, 500, 500, 510, 510, 510, 510, 510, 510, 510, 510, 500, 500, 500,
     500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500, 500,
-    500, 500, 495, 500, 500, 500, 500, 500, 500, 495, 500, 500, 500, 500, 500, 500, 500, 500, 490,
-    500, 500, 510, 510, 505, 500, 490,
+    500, 500, 495, 500, 500, 500, 500, 500, 500, 495, 490, 500, 500, 500, 500, 500, 500, 490, 495,
+    490, 500, 510, 510, 505, 490, 495,
 ];
 
 const KING_SQUARE_RATINGS: [i32; 64] = [
@@ -46,7 +46,7 @@ fn rate_pos(board: &Board) -> i32 {
         (board.black_bishops, &BISHOP_SQUARE_RATINGS, false),
         (board.white_rooks, &ROOK_SQUARE_RATINGS, true),
         (board.black_rooks, &ROOK_SQUARE_RATINGS, false),
-        (board.white_kings, &KNIGHT_SQUARE_RATINGS, true),
+        (board.white_kings, &KING_SQUARE_RATINGS, true),
         (board.black_kings, &KING_SQUARE_RATINGS, false),
     ];
 
@@ -56,9 +56,9 @@ fn rate_pos(board: &Board) -> i32 {
             let square = other.trailing_zeros();
             other ^= 1 << square;
             if color {
-                rating += rating_table[square as usize];
+                rating += rating_table[63 - square as usize];
             } else {
-                rating -= rating_table[63 - square as usize];
+                rating -= rating_table[square as usize];
             }
         }
     }
@@ -66,17 +66,50 @@ fn rate_pos(board: &Board) -> i32 {
     rating += (board.white_queens.count_ones() * 900) as i32;
     rating -= (board.black_queens.count_ones() * 900) as i32;
 
-    return rating;
+    return if board.turn == Color::WHITE {
+        rating
+    } else {
+        -rating
+    };
+}
+
+fn quiescence_search(board: &Board, _alpha: i32, _beta: i32) -> i32 {
+    let original = rate_pos(board);
+    let mut best = original;
+    if original >= _beta {
+        return original;
+    }
+
+    let mut alpha = if _alpha < original { original } else { _alpha };
+
+    for pair in board.get_next_move_boards() {
+        if pair.0.ep_move
+            || match board.get_piece_at(pair.0.to) {
+                (Piece::NONE, Color::WHITE) => false,
+                (Piece::NONE, Color::BLACK) => false,
+                _ => true,
+            }
+        {
+            let rating = -quiescence_search(&pair.1, -_beta, alpha);
+            if rating >= _beta {
+                return rating;
+            }
+            if rating > best {
+                best = rating;
+            }
+            if rating > alpha {
+                alpha = rating;
+            }
+        }
+    }
+
+    return best;
 }
 
 fn alpha_beta(board: &Board, depth: i32, _alpha: i32, _beta: i32) -> i32 {
     let mut alpha = _alpha;
     if depth == 0 {
-        if board.turn == Color::WHITE {
-            return rate_pos(board);
-        } else {
-            return -rate_pos(board);
-        }
+        return quiescence_search(board, alpha, _beta);
     }
 
     let mut best = i32::MIN;
